e958026aa615fdc24faf25fbd75b1b73f211f50f
Relyzim <axel.royer98@gmail.com>
2026-07-21 15:09:40 +0200

    maj logo

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    Pense à incrémenter APP_VERSION à chaque mise à jour de l'appli.
    ══════════════════════════════════════════════════════════════════ */
 
-const APP_VERSION = 'v4';
+const APP_VERSION = 'v5';
 const SHELL_CACHE = 'biblio-shell-' + APP_VERSION;
 const COVER_CACHE = 'biblio-covers-' + APP_VERSION;
 
