80d6c1b4f18f21ffee57daa14fd2a99367b5dd2e
Relyzim <axel.royer98@gmail.com>
2026-07-28 10:17:44 +0200

    v12.4

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    Pense à incrémenter APP_VERSION à chaque mise à jour de l'appli.
    ══════════════════════════════════════════════════════════════════ */
 
-const APP_VERSION = 'v12.3';
+const APP_VERSION = 'v12.4';
 const SHELL_CACHE = 'biblio-shell-' + APP_VERSION;
 const COVER_CACHE = 'biblio-covers-' + APP_VERSION;
 
